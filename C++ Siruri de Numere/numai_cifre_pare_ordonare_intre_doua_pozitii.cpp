e5b923fd6cb4d7413cde02325842294e8d116d45
StetFaraon <119285183+StetFaraon@users.noreply.github.com>
2023-10-11 12:31:44 +0100

    Update numai_cifre_pare_ordonare_intre_doua_pozitii.cpp

--- C++ Siruri de Numere/numai_cifre_pare_ordonare_intre_doua_pozitii.cpp
+++ C++ Siruri de Numere/numai_cifre_pare_ordonare_intre_doua_pozitii.cpp
@@ -23,22 +23,22 @@ int main() {
     y = 0;
 
     for (i = 1; i <= n; i++) {
-    x=0;
-    t=a[i];
+    x = 0;
+    t = a[i];
     while(t!=0){
         if (t % 2 == 1){
             x = 1;
             break;
         }
-        t=t/10;
+        t = t/10;
     }
     if(x == 0){
         cout << a[i] << " ";
-        y=1;
+        y = 1;
         }
     }
 
-    if(y==0)cout << "NU EXISTA NUMERE NUMAI CU CIFRE PARE";
+    if(y == 0)cout << "NU EXISTA NUMERE NUMAI CU CIFRE PARE";
 
     for (i = p1; i <= p2; i++) {
         for (j = p1; j < p2; j++){
