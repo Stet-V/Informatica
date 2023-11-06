534a44c9c44df42ec642f019b4d48c8aede3297b
Stet <vasile.stet@liceulborsa.ro>
2023-11-06 13:38:12 +0200

    functii introducere

---
+++ C++ Functii/introducere.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void suma(int a, int b, int &s){
+
+    s = a + b;
+    cout << s;
+
+}
+
+int main(){
+
+    int x, y, z;
+
+    cin >> x >> y;
+
+    suma(x, y, z);
+
+    // cout << z;
+
+}
--- C++ Matrici/numere_prime.cpp
+++ C++ Matrici/numere_prime.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int main() {
