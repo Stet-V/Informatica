52846dadbc667e974680bd6a018e16883212f8fd
Stet-V <119285183+Stet-V@users.noreply.github.com>
2023-11-10 12:26:54 +0000

    Update numar_numere_prime.cpp

--- C++ Functii/numar_numere_prime.cpp
+++ C++ Functii/numar_numere_prime.cpp
@@ -6,7 +6,9 @@ void prim(int a, int &c){
 
     c = 0;
 
-    for (int i = 2; i <= a/2; i++){
+    if (a == 1) c++;
+
+    for (int i = 2; i <= a / 2; i++){
             
         if (a % i == 0) c++;
 
