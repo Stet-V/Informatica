94f2baa523c7b5bb9ce74bb4d5e9a320351a78c1
Stet-V <119285183+Stet-V@users.noreply.github.com>
2023-11-10 15:36:26 +0000

    Update numar_numere_prime.cpp

--- C++ Functii/numar_numere_prime.cpp
+++ C++ Functii/numar_numere_prime.cpp
@@ -1,41 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
-void prim(int a, int &c){
-
+void prim(int a, int &c) {
     c = 0;
-
     if (a == 1) c++;
-
-    for (int i = 2; i <= a / 2; i++){
-            
+    for (int i = 2; i <= a / 2; i++) {
         if (a % i == 0) c++;
-
-    }  
-
-}
-
-int main(){
-
-    int A[50], n, x, y;
-
-    cin >> n;
-
-    y = 0;
-
-    for (int i = 1; i <= n; i++){
-
-        cin >> A[i];
-
-        x = 0;
-
-        prim(A[i], x);
-
-        if (x == 0) y++;
-
     }
-
+}
+int main() {
+    int A[50], n, x, y;
+    cin >> n;
+    y = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> A[i];
+        x = 0;
+        prim(A[i], x);
+        if (x == 0) y++;
+    }
     cout << y;
-
 }
