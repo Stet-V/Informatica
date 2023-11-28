085b625a8b9bc31f7e33b683ce7e3e19321e42ce
Stet-V <119285183+Stet-V@users.noreply.github.com>
2023-11-28 14:59:17 +0200

    Create numar_abundent.cpp

---
+++ C++ Functii/numar_abundent.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int abundent(int n){
+    float x = 0, y;
+    int j, i;
+    for (int i = 1; i <= n; i++) if (n % i == 0) x = x + i;
+    for (int i = 1; i <= n - 1; i++){
+        y = 0;
+        for (int j = 1; j <= i; j++) if (i % j == 0){
+            y = y + j;
+            if (y / i >= x / n) return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){   
+    int n;
+    cin >> n;
+    cout << abundent(n);
+}
