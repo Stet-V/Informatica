e305812d971c23dea44513f18e722796e621baac
Stet-V <119285183+Stet-V@users.noreply.github.com>
2024-07-04 00:17:05 +0300

    Create exersare.cpp

---
+++ C++ Siruri de Caractere/exersare.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char s[50], rez[50];
+    strcpy(s, "sir de caractere");
+    strcat(s, " concatenare, adica adaugare in sir");
+    char *n = strchr(s, ' ');
+    while(n != NULL){
+        cout << n[2] << n << endl;
+        n = strchr(n + 1, ' ');
+    }
+    char *t = strtok(s, " ");
+    while(t != NULL){
+        cout << strlen(t) << " ";
+        if (!strchr("aeiou", t[0])){
+            strcat(rez, t);
+            strcat(rez, " ");
+        }
+        t = strtok(NULL, " ,");
+    }
+    cout << rez << endl;
+    return 0;
+}
